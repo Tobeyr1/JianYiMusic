625946d0c5ef92bb7476c930d7f14eee28151d55
tober_r1 <2389704501@qq.com>
2022-06-22 01:27:53 +0800

    add home banner

---
+++ app/src/main/java/com/tobery/personalmusic/util/GeneratedAppGlideModule.java
@@ -0,0 +1,12 @@
+package com.tobery.personalmusic.util;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public class GeneratedAppGlideModule extends AppGlideModule {
+    @Override
+    public boolean isManifestParsingEnabled() {
+        return false;
+    }
+}
