5108b0119e806920801a138aece268ff286ba7b7
tober_r1 <2389704501@qq.com>
2022-07-01 00:22:35 +0800

    add web activity

--- app/src/main/java/com/tobery/personalmusic/ui/WebActivity.java
+++ app/src/main/java/com/tobery/personalmusic/ui/WebActivity.java
@@ -2,24 +2,13 @@ package com.tobery.personalmusic.ui;
 
 import static com.tobery.personalmusic.util.Constant.BANNER_URI;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.webkit.WebChromeClient;
-import android.webkit.WebView;
 
 import com.tobery.personalmusic.BaseActivity;
 import com.tobery.personalmusic.databinding.ActivityWebBinding;
 import com.tobery.personalmusic.util.ClickUtil;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
-
-import java.io.IOException;
-import java.io.InputStream;
 
 public class WebActivity extends BaseActivity {
 
@@ -35,19 +24,15 @@ public class WebActivity extends BaseActivity {
     private void initView() {
         binding.web.setSettings(this);
         String url = getIntent().getStringExtra(BANNER_URI);
+        binding.web.getTitle(title ->{
+            binding.viewTitle.tvTitle.setText(title);
+            binding.viewTitle.tvTitle.setVisibility(View.VISIBLE);
+            return null;
+        });
         binding.web.setUrl(url);
         binding.viewTitle.ivBack.setOnClickListener(view -> {
             if (ClickUtil.enableClick()){
                 finish();
-            }
-        });
-        binding.web.setCromeClient(binding.web.getWebChromeClient());
-        binding.web.setCromeClient(new WebChromeClient(){
-            @Override
-            public void onReceivedTitle(WebView view, String title) {
-                super.onReceivedTitle(view, title);
-                binding.viewTitle.tvTitle.setText(title);
-                binding.viewTitle.tvTitle.setVisibility(View.VISIBLE);
             }
         });
     }
@@ -67,6 +52,6 @@ public class WebActivity extends BaseActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        binding.web.setDestory();
+        binding.web.setDestroy();
     }
 }
