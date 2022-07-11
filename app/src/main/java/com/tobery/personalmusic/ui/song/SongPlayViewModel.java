b4e52817b7da0a4d4c3fe6b9db36d5c41efd94f4
liu_zirui <13115231059@163.com>
2022-07-11 20:43:17 +0800

    add playing function

--- app/src/main/java/com/tobery/personalmusic/ui/song/SongPlayViewModel.java
+++ app/src/main/java/com/tobery/personalmusic/ui/song/SongPlayViewModel.java
@@ -5,12 +5,15 @@ import static com.tobery.personalmusic.util.Constant.KEY_MAIN_UI;
 
 import androidx.databinding.ObservableField;
 import androidx.databinding.ObservableInt;
+import androidx.databinding.ObservableLong;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.SavedStateHandle;
 import androidx.lifecycle.ViewModel;
 
 import com.google.gson.Gson;
 import com.tobery.livedata.call.livedatalib.ApiResponse;
+import com.tobery.musicplay.MusicPlay;
 import com.tobery.personalmusic.entity.LoginEntity;
 import com.tobery.personalmusic.entity.LrcEntry;
 import com.tobery.personalmusic.entity.LyricEntity;
@@ -40,15 +43,34 @@ public class SongPlayViewModel extends ViewModel {
 
     public Boolean isShowLrc = false;
 
+    public ObservableField<String> currentSongUrl = new ObservableField<>("");
+
+    public ObservableLong currentSongId = new ObservableLong();
+
     public SongPlayViewModel(SavedStateHandle savedStateHandle) {
         this.state = savedStateHandle;
-        ui = state.get(KEY_MAIN_UI) == null ? new UserInfoUi(new ObservableField<>(""), new ObservableField<>(""), new ObservableInt(0), new ObservableField<>(""), new ObservableField<>(""), new ObservableField<>("")) : state.get(KEY_MAIN_UI);
+       // ui = state.get(KEY_MAIN_UI) == null ? new UserInfoUi(new ObservableField<>(""), new ObservableField<>(""), new ObservableInt(0), new ObservableField<>(""), new ObservableField<>(""), new ObservableField<>("")) : state.get(KEY_MAIN_UI);
     }
 
 
-    public LiveData<ApiResponse<LyricEntity>> getLyric(long songId) {
-        return RetrofitUtils.getmApiUrl().getLyric(songId);
+    public LiveData<ApiResponse<LyricEntity>> getLyric() {
+        return RetrofitUtils.getmApiUrl().getLyric(currentSongId.get());
     }
 
+    public void musicPlayOrPause(){
+        if (MusicPlay.isPlaying()){
+            MusicPlay.pauseMusic();
+        }else {
+            MusicPlay.restoreMusic();
+        }
+    }
+
+    public void next(){
+        MusicPlay.skipToNext();
+    }
+
+    public void previous(){
+        MusicPlay.skipToPrevious();
+    }
 
 }
