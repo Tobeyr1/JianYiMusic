c5f445b887e712f379179b778e970e434a1379bd
liu_zirui <liu_zirui@stationdm.com>
2023-12-22 15:56:05 +0800

    解决实体类中类型错误的bug

--- app/src/main/java/com/tobery/personalmusic/ui/home/menu/UserInfoUi.java
+++ app/src/main/java/com/tobery/personalmusic/ui/home/menu/UserInfoUi.java
@@ -2,6 +2,7 @@ package com.tobery.personalmusic.ui.home.menu;
 
 import androidx.databinding.ObservableField;
 import androidx.databinding.ObservableInt;
+import androidx.databinding.ObservableLong;
 
 import java.io.Serializable;
 
@@ -9,13 +10,13 @@ public class UserInfoUi implements Serializable {
 
     public ObservableField<String> imageUrl;
     public ObservableField<String> nickname;//昵称
-    public ObservableInt userId;
+    public ObservableLong userId;
     public ObservableField<String> signature;//个签
     public ObservableField<String> follows;//关注
     public ObservableField<String> followeds;//粉丝
     public ObservableField<String> level;//用户等级
 
-    public UserInfoUi(ObservableField<String> imageUrl, ObservableField<String> nickname, ObservableInt userId,ObservableField<String> signature, ObservableField<String> follows, ObservableField<String> followeds,ObservableField<String> level) {
+    public UserInfoUi(ObservableField<String> imageUrl, ObservableField<String> nickname, ObservableLong userId,ObservableField<String> signature, ObservableField<String> follows, ObservableField<String> followeds,ObservableField<String> level) {
         this.nickname = nickname;
         this.imageUrl = imageUrl;
         this.userId = userId;
